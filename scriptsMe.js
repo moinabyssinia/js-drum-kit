1fbd0ffa5268baadd625bae92dfbe3ff7afd957f
Michael Tadesse <49251957+moinabyssinia@users.noreply.github.com>
2020-07-25 00:01:40 -0400

    Add files via upload

---
+++ scriptsMe.js
@@ -0,0 +1,10 @@
+const letterF = document.querySelector('.first');
+
+
+
+letterF.addEventListener('keydown', function(){
+    // // if (event.code === 70){
+        console.log('pressed');
+        // letterF.classList.toggle('.pressed');
+    // }
+});
