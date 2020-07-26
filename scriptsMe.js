6f9cb3b400598995dce297b3dbeb63c2f46c1223
moinabyssinia <michaelg.tadesse@gmail.com>
2020-07-26 08:06:39 -0400

    working on styling logic

--- scriptsMe.js
+++ scriptsMe.js
@@ -1,10 +1,17 @@
-const letterF = document.querySelector('.first');
+const allLetters = document.querySelectorAll('.letter');
+
+// allLetters.forEach(function(letter){
+//     console.log("from node list", letter.dataset.key);
+// })
 
 
+window.addEventListener('keydown', function(event){
+    // console.log("from key press", event.keyCode);
+    allLetters.forEach(function(letter){
+        if (event.keyCode === Number(letter.dataset.key)){
+            console.log(event.keyCode,Number(letter.dataset.key));
+            console.log('success!');
+        }
+    })
 
-letterF.addEventListener('keydown', function(){
-    // // if (event.code === 70){
-        console.log('pressed');
-        // letterF.classList.toggle('.pressed');
-    // }
 });
