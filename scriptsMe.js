68c468947cbb1522869208891dd49021ecaa931c
moinabyssinia <michaelg.tadesse@gmail.com>
2020-07-26 09:27:41 -0400

    finished sound logic

--- scriptsMe.js
+++ scriptsMe.js
@@ -1,16 +1,56 @@
 const allLetters = document.querySelectorAll('.letter');
 
-// allLetters.forEach(function(letter){
-//     console.log("from node list", letter.dataset.key);
-// })
+/* define sound objects for letters */
+let keyData ={
+    70: {
+        sound: new Howl({
+            src: ['sounds/ride.wav']
+        })
+    },
+    76: {
+        sound: new Howl({
+            src: ['sounds/tom.wav']
+        })
+    },
+    79: {
+        sound: new Howl({
+            src: ['sounds/tink.wav']
+        })
+    },
+    82: {
+        sound: new Howl({
+            src: ['sounds/snare.wav']
+        })
+    },
+    73: {
+        sound: new Howl({
+            src: ['sounds/openhat.wav']
+        })
+    },
+    68: {
+        sound: new Howl({
+            src: ['sounds/kick.wav']
+        })
+    },
+    65: {
+        sound: new Howl({
+            src: ['sounds/clap.wav']
+        })
+    }
+}
 
 
+/* apply transition on keys when typed */
 window.addEventListener('keydown', function(event){
-    // console.log("from key press", event.keyCode);
     allLetters.forEach(function(letter){
         if (event.keyCode === Number(letter.dataset.key)){
-            console.log(event.keyCode,Number(letter.dataset.key));
-            console.log('success!');
+            letter.classList.add('playing');
+            // play corresnponding audio file
+            keyData[event.keyCode].sound.play();
+            //remove the playing class when transition ends
+            letter.addEventListener('transitionend', function(){
+                letter.classList.remove('playing');
+            })
         }
     })
 
